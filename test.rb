41da362ce098a09d242fa669122f886bbb16d94d
shogo2421 <2421falsecarp@gmail.com>
2020-10-17 22:59:07 +0900

    test

---
+++ test.rb
@@ -0,0 +1,24 @@
+outputs = [
+  'puts "Hello World!"',
+  'echo "Hello World!";',
+  'System.out.println("Hello World!");'
+]
+
+languages = [
+  "Ruby",
+  "PHP",
+  "Java"
+]
+
+puts "様々な言語のHello, World"
+puts
+languages.each do |language|
+  case language
+  when "Ruby"
+    puts "#{language}:#{outputs[0]}"
+  when "PHP"
+    puts "#{language}:#{outputs[1]}"
+  when "Java"
+    puts "#{language}:#{outputs[2]}"
+  end
+end
